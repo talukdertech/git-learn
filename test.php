a2c21c4ad6a790506a65ad8ab6ce6036d29b132b
Shahabuddin <sabudss74@gmail.com>
2025-04-12 18:10:34 +0600

    Tabulized Version Added

--- test.php
+++ test.php
@@ -2,3 +2,14 @@
 $students=["Minhaz","Azharul","Shakil"];
 echo "Hello This is my First Git Learn";
 ?>
+<table border="1" width="200" cellpadding="5" cellspacing="0">
+    <tr>
+        <th>Students Name</th>
+    </tr>
+    <?php
+    foreach($newStudents as $student){
+        echo "<tr><td><center>$student</center></td></tr>";
+    }
+    ?>
+</table>
+
